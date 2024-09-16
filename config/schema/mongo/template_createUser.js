95f916411b96d30ebe8f5491f22987eec23fd086
perceval <martial.goehry@gmail.com>
2024-09-16 08:33:07 +0200

    Ajout du template des conf

--- config/schema/mongo/template_createUser.js
+++ config/schema/mongo/template_createUser.js
@@ -1,7 +1,7 @@
 print("Ajout de l'utilisateur de service");
 db.getSiblingDB("errol").runCommand({
   createUser: "mongo_errol",
-  pwd: "Azerty1234",
+  pwd: "<CHANGE ME>",
   roles: [
     { role: "readWrite", db: "errol" },
   ],
